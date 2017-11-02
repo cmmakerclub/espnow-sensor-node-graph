4ae8927b3d206f90a3e1af9e525b690201919559
Compiler Exe <compilerexe@gmail.com>
2017-11-02 21:25:06 +0700

    add paho-mqtt.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import paho from 'paho-mqtt/mqttws31'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
